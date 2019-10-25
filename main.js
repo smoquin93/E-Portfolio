96e236159432b6c54029a4474a4132646d411592
Shannon Moquin <shannon_moquin@yahoo.com>
2019-10-24 19:52:03 -0600

    Complete change to site
    Complete redesign.

--- main.js
+++
@@ -1,7 +0,0 @@
-var colors = ['#f9feff', '#faf4ff', '#f9fff9'];
-
-var changeBackground = function() {
-	document.body.style.background = colors[Math.floor(Math.random()*colors.length)];
-};
-
-changeBackground();
